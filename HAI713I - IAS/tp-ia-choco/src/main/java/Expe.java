8e97282086fa51428f0b87bebbbb2afc5e39026d
thibaud.paulin01@etu.umontpellier.fr <thibaud.paulin@gmail.com>
2025-10-14 16:32:16 +0200

    oui

---
+++ HAI713I - IAS/tp-ia-choco/src/main/java/Expe.java
@@ -0,0 +1,96 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.constraints.extension.Tuples;
+import org.chocosolver.solver.variables.IntVar;
+
+public class Expe {
+
+	private static Model lireReseau(BufferedReader in) throws Exception{
+			Model model = new Model("Expe");
+			int nbVariables = Integer.parseInt(in.readLine());				// le nombre de variables
+			int tailleDom = Integer.parseInt(in.readLine());				// la valeur max des domaines
+			IntVar []var = model.intVarArray("x",nbVariables,0,tailleDom-1); 	
+			int nbConstraints = Integer.parseInt(in.readLine());			// le nombre de contraintes binaires		
+			for(int k=1;k<=nbConstraints;k++) { 
+				String chaine[] = in.readLine().split(";");
+				IntVar portee[] = new IntVar[]{var[Integer.parseInt(chaine[0])],var[Integer.parseInt(chaine[1])]}; 
+				int nbTuples = Integer.parseInt(in.readLine());				// le nombre de tuples		
+				Tuples tuples = new Tuples(new int[][]{},true);
+				for(int nb=1;nb<=nbTuples;nb++) { 
+					chaine = in.readLine().split(";");
+					int t[] = new int[]{Integer.parseInt(chaine[0]), Integer.parseInt(chaine[1])};
+					tuples.add(t);
+				}
+				model.table(portee,tuples).post();	
+			}
+			in.readLine();
+			return model;
+	}	
+		
+			
+	public static void main(String[] args) throws Exception{
+        System.out.println("BENCH.TXT");
+        HashMap<String, Integer> resSol =  new HashMap<>();
+		String ficName = "bench.txt";
+        resSol.put(ficName,0);
+		int nbRes=3;
+		BufferedReader readFile = new BufferedReader(new FileReader(ficName));
+		for(int nb=1 ; nb<=nbRes; nb++) {
+			Model model=lireReseau(readFile);
+			if(model==null) {
+				System.out.println("Problème de lecture de fichier !\n");
+				return;
+			}
+			System.out.println("Réseau lu "+nb+" :\n"+model+"\n\n");
+            // Calcul de la première solution
+            if(model.getSolver().solve()) {
+                System.out.println("\n\n*** Première solution ***");
+                System.out.println(model);
+                resSol.put(ficName,resSol.get(ficName)+1);
+            }
+		}
+        System.out.println("BENCHSATISF.TXT");
+        ficName = "benchSatisf.txt";
+        resSol.put(ficName,0);
+        readFile = new BufferedReader(new FileReader(ficName));
+        for(int nb=1 ; nb<=nbRes; nb++) {
+            Model model=lireReseau(readFile);
+            if(model==null) {
+                System.out.println("Problème de lecture de fichier !\n");
+                return;
+            }
+            System.out.println("Réseau lu "+nb+" :\n"+model+"\n\n");
+            // Calcul de la première solution
+            if(model.getSolver().solve()) {
+                System.out.println("\n\n*** Première solution ***");
+                System.out.println(model);
+                resSol.put(ficName,resSol.get(ficName)+1);
+            }
+        }
+        System.out.println("BENCHINSAT.TXT");
+        ficName = "benchInsat.txt";
+        resSol.put(ficName,0);
+        readFile = new BufferedReader(new FileReader(ficName));
+        for(int nb=1 ; nb<=nbRes; nb++) {
+            Model model=lireReseau(readFile);
+            if(model==null) {
+                System.out.println("Problème de lecture de fichier !\n");
+                return;
+            }
+            System.out.println("Réseau lu "+nb+" :\n"+model+"\n\n");
+            // Calcul de la première solution
+            if(model.getSolver().solve()) {
+                System.out.println("\n\n*** Première solution ***");
+                System.out.println(model);
+                resSol.put(ficName,resSol.get(ficName)+1);
+            }
+        }
+		return;	
+	}
+	
+}
